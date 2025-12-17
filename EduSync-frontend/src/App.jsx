25f6eba7e1b479f217ab1718f8f6077930070030
Ayushmangiri <ayushmangiri@gmail.com>
2025-12-17 13:09:33 +0530

    Added the List of Students

--- EduSync-frontend/src/App.jsx
+++ EduSync-frontend/src/App.jsx
@@ -1,10 +1,13 @@
 import StudentsForm from "./components/StudentsForm";
 import StudentsList from "./components/StudentsList";
 function App() {
-  return (
-    <div className="App">
-      <StudentsForm />
+return (
+    <div>
+        <h1>EduSync</h1>
+        <StudentsForm />
         <StudentsList />
     </div>
-  );
+);
+
 }
+export default App;
---
+++ EduSync-frontend/src/components/StudentsList.jsx
@@ -0,0 +1,23 @@
+function StudentsList(){
+
+    const students = [
+        { id: 1, name: "Alice Johnson", age: 20, major: "Computer Science" },
+        { id: 2, name: "Bob Smith", age: 22, major: "Mathematics" },
+        { id: 3, name: "Charlie Brown", age: 21, major: "Physics" },
+    ];
+
+    return (
+        <div>
+            <h1>Students List</h1>
+            <ul>
+                {students.map((student) => (
+                    <li key={student.id}>
+                        Name :{student.name} - Age: {student.age}, Major: {student.major}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default StudentsList;
