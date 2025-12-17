7a221126a444ffa517cd4aeb67401ad658b6e89e
Ayushmangiri <ayushmangiri@gmail.com>
2025-12-17 12:28:21 +0530

    added components

---
+++ EduSync-frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import StudentsForm from "./components/StudentsForm";
+import StudentsList from "./components/StudentsList";
+function App() {
+  return (
+    <div className="App">
+      <StudentsForm />
+        <StudentsList />
+    </div>
+  );
+}
