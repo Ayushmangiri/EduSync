6730ac969a02f4ba97c177c7f2f8570164683247
Ayushmangiri <ayushmangiri@gmail.com>
2025-12-18 12:21:25 +0530

    Added form in students table

--- EduSync-frontend/src/App.jsx
+++ EduSync-frontend/src/App.jsx
@@ -1,13 +1,25 @@
 import StudentsForm from "./components/StudentsForm";
+import { useState } from "react";
 import StudentsList from "./components/StudentsList";
 function App() {
+const [students, setStudents] = useState([
+    { id: 1, name: "Alice Johnson", age: 20, major: "Computer Science" },
+    { id: 2, name: "Bob Smith", age: 22, major: "Mathematics" },
+    { id: 3, name: "Charlie Brown", age: 21, major: "Physics" },
+]);
+ const addStudent = (student) => {
+    setStudents([...students, { ...student, id: students.length + 1 }]);
+};  
+const deleteStudent = (id) => {
+    setStudents(students.filter((student) => student.id !== id));
+};
 return (
     <div>
         <h1>EduSync</h1>
-        <StudentsForm />
-        <StudentsList />
+        <StudentsForm students={students} setStudents={setStudents} />
+        <StudentsList students={students} />
     </div>
 );
-
 }
+
 export default App;
---
+++ EduSync-frontend/src/components/StudentsForm.jsx
@@ -0,0 +1,12 @@
+function StudentsForm() {
+    return (
+        <div>
+            <h2> Add Students</h2>
+            <input type="text" placeholder="Student Name" />
+            <input type ="text" placeholder="course" />
+            <button> Add </button>
+        </div>
+    );
+}
+
+export default StudentsForm;
